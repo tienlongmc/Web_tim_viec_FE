b2f6af55b0cec0957a1619434f9356fc5e2dfbcd
xthanh12 <vuxuanthanh1203@gmail.com>
2025-10-16 14:49:55 +0700

    Refactor Code

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,29 @@
-import { defineConfig, loadEnv, type PluginOption } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import path from 'path';
+import { defineConfig, loadEnv, type PluginOption } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 import { visualizer } from "rollup-plugin-visualizer";
-import dns from 'dns';
+import dns from "dns";
 
 //running on localhost instead of IP 127.0.0.1
 // https://vitejs.dev/config/server-options.html#server-host
-dns.setDefaultResultOrder('verbatim')
+dns.setDefaultResultOrder("verbatim");
 
 // https://vitejs.dev/config/
 // https://v2.vitejs.dev/config/#environment-variables
 export default defineConfig(({ command, mode }) => {
   // Load env file based on `mode` in the current working directory.
   // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
-  const env = loadEnv(mode, process.cwd(), '')
+  const env = loadEnv(mode, process.cwd(), "");
   return {
     plugins: [
-      react(), 
+      react(),
       // visualizer() as PluginOption
     ],
+    esbuild: {
+      legalComments: "none", // bỏ cảnh báo license/comment
+    },
     server: {
-      port: parseInt(env.PORT)
+      port: parseInt(env.PORT),
     },
     resolve: {
       alias: {
@@ -31,5 +34,5 @@ export default defineConfig(({ command, mode }) => {
         pages: `${path.resolve(__dirname, "./src/pages/")}`,
       },
     },
-  }
-})
+  };
+});
